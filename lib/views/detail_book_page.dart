f13aac46419825fc8ba07079f7dd3f9f2ccfab8a
matdev90 <matdev90@localhost.localdomain>
2022-07-15 20:46:29 +0800

    add models detail response & list response

--- lib/views/detail_book_page.dart
+++ lib/views/detail_book_page.dart
@@ -1,27 +1,60 @@
+import 'dart:convert';
+
+import 'package:book_app/models/book_detail_response.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
 
 class DetailBookPage extends StatefulWidget {
-  const DetailBookPage({Key? key}) : super(key: key);
+  const DetailBookPage({
+    Key? key,
+    required this.isbn,
+  }) : super(key: key);
+  final String isbn;
 
   @override
   State<DetailBookPage> createState() => _DetailBookPageState();
 }
 
 class _DetailBookPageState extends State<DetailBookPage> {
+  BookDetailResponse? detailBook;
   fetchDetailBookApi() async {
-    var url = Uri.parse('https://api.itbook.store/1.0/books/9781484206485');
+    var url = Uri.parse('https://api.itbook.store/1.0/books/${widget.isbn}');
     var response = await http.get(url);
     print('Response status: ${response.statusCode}');
     print('Response body: ${response.body}');
+
+    if (response.statusCode == 200) {
+      final jsonDetail = jsonDecode(response.body);
+      detailBook = BookDetailResponse.fromJson(jsonDetail);
+      setState(() {});
+    }
 
     // print(await http.read(Uri.parse('https://example.com/foobar.txt')));
   }
 
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    fetchDetailBookApi();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Column();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Detail',
+        ),
+      ),
+      body: detailBook == null
+          ? Center(child: CircularProgressIndicator())
+          : Column(
+              children: [],
+            ),
+    );
   }
 }
